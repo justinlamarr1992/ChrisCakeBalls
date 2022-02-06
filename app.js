b1c2614a3c7dd791a44d2375a5b367202d464a61
Justin Williams <justinlamarr1992@icloud.com>
2022-02-05 18:47:59 -0600

    Website Revisions 1 Done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// NavBar
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navBarLinks = document.getElementsByClassName("navbar-links")[0];
 
@@ -5,10 +6,15 @@ toggleButton.addEventListener("click", () => {
   navBarLinks.classList.toggle("active");
 });
 
+// Modal
 var dataMap = new Map([
   [
     document.getElementById("aboutModalButton"),
     document.getElementById("aboutModal"),
+  ],
+  [
+    document.getElementById("galleryModalButton"),
+    document.getElementById("galleryModal"),
   ],
   [document.getElementById("cake1"), document.getElementById("cakeModal1")],
   [document.getElementById("cake2"), document.getElementById("cakeModal2")],
@@ -63,7 +69,110 @@ function doModal(anchor, popupbox) {
     }
   });
 }
+function galleryModal() {
+  // Gallery
+  var previous = document.getElementById("btnPrev");
+  var next = document.getElementById("btnNext");
+  var gallery = document.getElementById("image-gallery");
+  var pageIndicator = document.getElementById("page");
+  var galleryDots = document.getElementById("gallery-dots");
 
+  var images = [];
+  for (var i = 0; i < 19; i++) {
+    images.push({
+      title: "Image " + (i + 1),
+      source: "./images/CakeBalls/Image" + i + ".jpeg",
+    });
+  }
+
+  var perPage = 8;
+  var page = 1;
+  var pages = Math.ceil(images.length / perPage);
+
+  // Gallery Dots
+  for (var i = 0; i < pages; i++) {
+    var dot = document.createElement("button");
+    var dotSpan = document.createElement("span");
+    var dotNumber = (document = document.createTextNode(i + 1));
+    dot.classList.add("gallery-dot");
+    dot.setAttribute("data-index", i);
+    dotSpan.classList.add("sr-only");
+
+    dotSpan.appendChild(dotNumber);
+    dot.appendChild(dotSpan);
+
+    dot.addEventListener("click", function (e) {
+      var self = e.target;
+      goToPage(self.getAttribute("data-index"));
+    });
+    galleryDots.appendChild(dot);
+  }
+  // Prev Button
+  previous.addEventListener("click", function () {
+    if (page === 1) {
+      page = 1;
+    } else {
+      page--;
+      showImages();
+    }
+  });
+  // Next Button
+  next.addEventListener("click", function () {
+    if (page < pages) {
+      page++;
+      showImages();
+    }
+  });
+  // Jump to page
+  function goToPage(index) {
+    index = parseInt(index);
+    page = index + 1;
+    showImages();
+  }
+  // Load images
+  function showImages() {
+    while (gallery.firstChild) gallery.removeChild(gallery.firstChild);
+    var offset = (page - 1) * perPage;
+    var dots = document.querySelectorAll(".gallery-dot");
+    for (var i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("active");
+    }
+    dots[page - 1].classList.add("active");
+    for (var i = offset; i < offset + perPage; i++) {
+      if (images[i]) {
+        var template = document.createElement("div");
+        var title = document.createElement("p");
+        var titleText = document.createTextNode(images[i].title);
+        var img = document.createElement("img");
+        template.classList.add("template");
+
+        img.setAttribute("src", images[i].source);
+        img.setAttribute("alt", images[i].title);
+        title.appendChild(titleText);
+        template.appendChild(img);
+        // template.appendChild(title);
+        gallery.appendChild(template);
+      }
+    }
+    // animate images
+    var galleryItems = document.querySelectorAll(".template");
+    for (var i = 0; i < galleryItems.length; i++) {
+      var onAnimateItemIn = animateItemIn(i);
+      setTimeout(onAnimateItemIn, i * 100);
+    }
+    function animateItemIn(i) {
+      var item = galleryItems[i];
+      return function () {
+        item.classList.add("animate");
+      };
+    }
+    // Update page Indicator
+    pageIndicator.textContent = "Page " + page + " of " + pages;
+  }
+  showImages();
+}
+
+// Team
 function changeToChris() {
   var chris = document.getElementById("team-pic-chris");
   var jen = document.getElementById("team-pic-jen");
